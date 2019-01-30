6ed59844602a6114cc6c5fdc9ae0053fc6ef9e6a
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-01-30 15:11:15 -0400

    Practicando ciclo do while

--- ciclos/DoWhile.js
+++ ciclos/DoWhile.js
@@ -1,1 +1,13 @@
-DoWhile.js
+let contador = 0
+
+const llueve = () => Math.random() < .25
+
+do {
+	contador++
+} while(!llueve())
+
+if (contador === 1) {
+	console.log(`Fui a ver si llovía ${contador} vez`)
+} else {
+	console.log(`Fui a ver si llovía ${contador} veces`)
+}
