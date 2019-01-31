df330e2deebca55163aee6d4ae1315b62636ffb7
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-01-31 09:20:12 -0400

    Primera creacion de prototipo

---
+++ objetos/Prototipos.js
@@ -0,0 +1,11 @@
+function Persona(nombre, apellido){
+	this.name = nombre
+	this.lastName = apellido
+//	Atributo de   |  se iguala a los parametros que recibe.
+//	this (felix,  |
+//  en este caso) |
+//  llamado       |
+//  "name"      |
+}
+
+let felix = new Persona('Felix', 'Pacheco')
