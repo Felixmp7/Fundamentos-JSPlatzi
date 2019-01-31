a2d9cb9b474265aaf431fb3511d468f97de513ab
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-01-31 10:14:38 -0400

    Realizando tarea de Sacha

--- objetos/Prototipos.js
+++ objetos/Prototipos.js
@@ -1,4 +1,4 @@
-function Persona(nombre, apellido){
+/*function Persona(nombre, apellido){
 	this.name = nombre
 	this.lastName = apellido
 //	Atributo de   |  se iguala a los parametros que recibe.
@@ -16,4 +16,27 @@ Persona.prototype.saludar = function () {	//		Acá estoy creando un metodo salud
 
 let Felix = new Persona('Felix', 'Pacheco')
 let Gabriel = new Persona('Gabriel', 'Pacheco')
-let Jose = new Persona('José', 'Tapiquen')
+let Jose = new Persona('José', 'Tapiquen')*/
+
+
+
+// Tarea de Sacha
+
+function Persona(nombre, apellido, altura){
+	this.name = nombre
+	this.lastName = apellido
+	this.height = altura
+}
+
+
+Persona.prototype.decirSiEsAlto = function () {
+	if (this.height >= 1.8) {
+		console.log(`Me llamo ${this.name} ${this.lastName} y soy alto`)
+	} else {
+		console.log(`Me llamo ${this.name} ${this.lastName} y soy bajito`)
+	}
+} 
+
+let Felix = new Persona('Felix', 'Pacheco', 1.6)
+let Gabriel = new Persona('Gabriel', 'Pacheco', 1.8)
+let Jose = new Persona('José', 'Tapiquen', 1.82)
