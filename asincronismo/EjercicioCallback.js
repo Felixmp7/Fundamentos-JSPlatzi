e8b84c47c0322b19dde656a47552f4d83886e632
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-02-02 09:53:09 -0400

    Obteniendo personajes siguiendo el curso

--- asincronismo/EjercicioCallback.js
+++ asincronismo/EjercicioCallback.js
@@ -1,9 +1,10 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 
-let id = 0
+//Para mostrar 5 personajes a mi manera
+//let id = 0
 
-for(id = 1; id <= 5; id++){
+/*for(id = 1; id <= 5; id++){
 
 	const characterUrl = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
 	const options = { crossDomain: true }
@@ -12,6 +13,24 @@ for(id = 1; id <= 5; id++){
 	}
 
 	$.get(characterUrl, options, onCharacterResponse)
+}*/
+
+
+//Mostrando personajes a la manera de Sacha
+
+
+const options = { crossDomain: true }
+const onCharacterResponse = function (character){
+	console.log(`Hola, soy ${character.name}`)
 }
+
+function obtenerPersonaje(id){
+	const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+	$.get(url, options, onCharacterResponse)
+}
+
+obtenerPersonaje(1)
+obtenerPersonaje(2)	// En que orden crees que llegarán los datos?
+obtenerPersonaje(3)
 
 
