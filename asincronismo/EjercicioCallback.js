3101796da36afc41b149a066bc02ad9a300b04df
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-02-02 10:07:44 -0400

    Update

--- asincronismo/EjercicioCallback.js
+++ asincronismo/EjercicioCallback.js
@@ -2,9 +2,9 @@ const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 
 //Para mostrar 5 personajes a mi manera
-//let id = 0
 
-/*for(id = 1; id <= 5; id++){
+let id = 0
+for(id = 1; id <= 5; id++){
 
 	const characterUrl = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
 	const options = { crossDomain: true }
@@ -13,13 +13,13 @@ const PEOPLE_URL = 'people/:id'
 	}
 
 	$.get(characterUrl, options, onCharacterResponse)
-}*/
+}
 
 
 //Mostrando personajes a la manera de Sacha
 
 
-const options = { crossDomain: true }
+/*const options = { crossDomain: true }
 const onCharacterResponse = function (character){
 	console.log(`Hola, soy ${character.name}`)
 }
@@ -32,5 +32,5 @@ function obtenerPersonaje(id){
 obtenerPersonaje(1)
 obtenerPersonaje(2)	// En que orden crees que llegarán los datos?
 obtenerPersonaje(3)
-
+// Nunca sabremos en que orden van a llegar los datos de ésta manera*/
 
