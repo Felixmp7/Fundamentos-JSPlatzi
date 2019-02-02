4cc402c9cf0be2442f31b9df8358fbe17ab81e6d
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-02-02 18:33:01 -0400

    Encadenando promesas

---
+++ asincronismo/PromesasEncadenadas.js
@@ -0,0 +1,40 @@
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const options = { crossDomain: true }
+
+function obtenerPersonaje(id){
+	return new Promise((resolve,reject) => {
+		const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+
+		$.get(url,options, function(datos){
+			resolve(datos)
+		})
+		.fail(()=> reject(id))
+	})
+}
+
+function onError(id){
+	console.log(`Ocurrió un error inesperado al obtener al personaje ${id}`)
+}
+
+obtenerPersonaje(1)
+	.then(personaje1 => {
+		console.log(`Hola, soy ${personaje1.name}`)
+		return obtenerPersonaje(2)
+	})
+	.then(personaje2 => {
+		console.log(`Hola, soy ${personaje2.name}`)
+		return obtenerPersonaje(3)
+	})
+	.then(personaje3 => {
+		console.log(`Hola, soy ${personaje3.name}`)
+		return obtenerPersonaje(4)
+	})
+	.then(personaje4 => {
+		console.log(`Hola, soy ${personaje4.name}`)
+		return obtenerPersonaje(5)
+	})
+	.then(personaje4 => {
+		console.log(`Hola, soy ${personaje4.name}`)
+	})
+	.catch(onError)
