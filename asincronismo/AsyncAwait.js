2ed660f7fec28031d7cfcaf3115f9b0f9f957f8f
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-02-02 19:13:33 -0400

    Implementando y medio entendiendo async await

---
+++ asincronismo/AsyncAwait.js
@@ -0,0 +1,34 @@
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const options = { crossDomain: true }
+
+function obtenerPersonaje(id){
+	return new Promise((resolve,reject) => {
+		const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+
+		$.get(url,options, function(datos){
+			resolve(datos)
+		})
+		.fail(()=> reject(id))
+	})
+}
+
+function onError(id){
+	console.log(`Ocurrió un error inesperado al obtener al personaje ${id}`)
+}
+
+async function obtenerPersonajes(){
+	let ids = [1,2,3,4,5,6,7]
+	let promesas = ids.map(id => obtenerPersonaje(id))
+	//console.log(promesas)
+	try{
+
+		let personajes = await Promise.all(promesas)
+		console.log(personajes)
+
+	} catch(id){
+		onError(id)
+	}
+}
+
+obtenerPersonajes()
